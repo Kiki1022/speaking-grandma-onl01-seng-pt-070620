3587c2e74f112370edb131e0c335c26191a49e0e
Kinnari Shah <kinnari1022@yahoo.com>
2020-08-01 04:05:39 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,23 +1,20 @@
 
-
-
-
-# def speak_to_grandma (phrase)
+def speak_to_grandma (phrase)
   
-#   if phrase == "I LOVE YOU GRANDMA!"
-#       return "I LOVE YOU TOO PUMPKIN!"
+  if phrase == "I LOVE YOU GRANDMA!"
+      return "I LOVE YOU TOO PUMPKIN!"
       
 
-#   elsif phrase != phrase.upcase
-#       return "HUH?! SPEAK UP, SONNY!"
+  elsif phrase != phrase.upcase
+      return "HUH?! SPEAK UP, SONNY!"
   
   
-#   else phrase == phrase.upcase
-#       return "NO, NOT SINCE 1938!"
+  else phrase == phrase.upcase
+      return "NO, NOT SINCE 1938!"
   
   
-#   end
-# end
+  end
+end
     
 #     speak_to_grandma ("I LOVE YOU GRANDMA!")
 #     speak_to_grandma("Hi, Nana, how are you?")
